24c5ba5553a42ea8709a0f82dd7e3c46294bc55a
ISMAEL ARMANDO POLANCO GARCIA <1115279@est.intec.edu.do>
2023-04-06 01:03:35 -0400

    Cambio4

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        static string dataBaseStr = "server=localhost;database=RegistroAutobuses;integrated security=true";
+
+        SqlConnection conexion = new SqlConnection(dataBaseStr);
+
         public Form1()
         {
             InitializeComponent();
@@ -19,13 +24,29 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (txtPassword.Text != "")
-            {
-                Empleado empleado = new Empleado();
-                empleado.Show();
-                this.Hide();
-            }
+            conexion.Open();
 
+            string query = $"SELECT * FROM usuarios WHERE email='{txtEmail.Text}' AND contra = '{txtPassword.Text}'";
+
+            SqlCommand command = new SqlCommand(query, conexion);
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+
+                if (reader["privilegios"].ToString() == "true")
+                {
+                    Empleado empleado = new Empleado();
+                    empleado.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    Cliente cliente = new Cliente();
+                    cliente.Show();
+                    this.Hide();
+                }
+            }
+            conexion.Close();
         }
     }
 }
