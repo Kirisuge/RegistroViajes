7aaaf96617a3ff284832c6ba68f6156da1511f2d
Erick <Erick@DESKTOP-6E1C7M0>
2023-04-19 22:49:43 -0300

    agregar viaje agregado

--- WindowsFormsApp1/NuevoViaje.cs
+++ WindowsFormsApp1/NuevoViaje.cs
@@ -54,7 +54,88 @@ namespace WindowsFormsApp1
 
         private void btnCrearViaje_Click(object sender, EventArgs e)
         {
+            //Tomar los valores de las entradas de texto y horas
+            string lugarSalida = cBSalida.Text;
+            string destino = cBDestino.Text;
+            DateTime horarioSalida = dTPSalida.Value;
+            DateTime horarioLlegada = dTPLlegada.Value;
+            string capacidad = textCapacidad.Text.Trim();
+            string tarifa = textTarifa.Text.Trim();
 
+            //Hacer las comprobaciones pertinentes
+            //Comprobar que ninguno este vacio
+            if (lugarSalida == string.Empty || destino == string.Empty || horarioSalida == DateTime.Now || horarioLlegada == DateTime.Now || capacidad == string.Empty || tarifa == string.Empty)
+            {
+                RegistroInvalido("Favor completar todos los campos");
+                return;
+            }
+
+            // Validar que el lugar de salida no es igual al lugar de llegada
+            if (lugarSalida == destino)
+            {
+                RegistroInvalido("La sucursal de salida y la de llegada para el viaje no pueden ser las mismas.");
+                return;
+            }
+
+            //Compara las dos fechas
+            if (CompararFechas(horarioSalida,horarioLlegada) >= 0)
+            {
+                RegistroInvalido("Favor ingrese fechas válidas.");
+                return;
+            }
+
+            //Comprobar que la tarifa y la capacidad son numeros enteros
+            if (ComprobarSiEsNumero(capacidad) == false || ComprobarSiEsNumero(tarifa) == false)
+            {
+                RegistroInvalido("Favor ingrese cantidades válidas");
+                return;
+            }
+
+            //Crea objeto base de datos y se llama al metodo para agregar un viaje 
+            BaseDatos db = new BaseDatos();
+            db.AgregarNuevoViaje(lugarSalida, destino,horarioSalida,horarioLlegada,int.Parse(capacidad),int.Parse(tarifa));
+
+            //Muestra un mensaje y limpia los campos
+            RegistroValido();
+        }
+
+        private void RegistroValido()
+        {
+            MessageBox.Show("¡Viaje creado exitosamente!");
+            LimpiarCampos();
+        }
+
+        //Mensaje de error
+        private void RegistroInvalido(string mensaje)
+        {
+            MessageBox.Show(mensaje);
+        }
+
+        //Devuelve true si la cadena es un numero, de lo contrario, false
+        private bool ComprobarSiEsNumero(string cadena)
+        {
+            int numero;
+            return int.TryParse(cadena, out numero);
+        }
+
+        //devuelve un numeor menor que cero si salida es anterior a llegada
+        //devuelve 0 si coinciden
+        //devuelve un numero mayor a cero si salida es posterior a llegada
+        private int CompararFechas(DateTime salida, DateTime llegada)
+        {
+            return DateTime.Compare(salida, llegada);
+        }
+
+        //Reestablece los valores de los campos
+        private void LimpiarCampos()
+        {
+            cBSalida.Text= string.Empty;
+            cBDestino.Text= string.Empty;
+            dTPLlegada.Value = DateTime.Now;
+            dTPSalida.Value= DateTime.Now;
+            textCapacidad.Clear();
+            textTarifa.Clear();
         }
     }
 }
+
